fe7280102ae6745e1b6358dac97d46d5cd2075c3
张青明 <zhangqingming@codinghacker.com>
2015-09-16 18:18:37 +0800

    初始化工程

---
+++ CircleMusicSwift/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Alamofire
+#define COCOAPODS_POD_AVAILABLE_Alamofire
+#define COCOAPODS_VERSION_MAJOR_Alamofire 1
+#define COCOAPODS_VERSION_MINOR_Alamofire 3
+#define COCOAPODS_VERSION_PATCH_Alamofire 1
+
+// Kingfisher
+#define COCOAPODS_POD_AVAILABLE_Kingfisher
+#define COCOAPODS_VERSION_MAJOR_Kingfisher 1
+#define COCOAPODS_VERSION_MINOR_Kingfisher 5
+#define COCOAPODS_VERSION_PATCH_Kingfisher 0
+
+// SwiftyJSON
+#define COCOAPODS_POD_AVAILABLE_SwiftyJSON
+#define COCOAPODS_VERSION_MAJOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_MINOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_PATCH_SwiftyJSON 1
+
+// pop
+#define COCOAPODS_POD_AVAILABLE_pop
+#define COCOAPODS_VERSION_MAJOR_pop 1
+#define COCOAPODS_VERSION_MINOR_pop 0
+#define COCOAPODS_VERSION_PATCH_pop 7
+
